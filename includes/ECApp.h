725cc96d270b179044321311dd8c9c62719ff701
Achraf Lebrazi <achraf.lebrazi@hillrom.com>
2022-11-12 23:28:12 +0100

    Project builds now with Cmake using FetchContent module to retrieve SDL2 dynamically:
    - Cleaned the directories' structure
    - Deleted Windows API dependencies
    - Removed VS files.

---
+++ includes/ECApp.h
@@ -0,0 +1,32 @@
+#ifndef _EIGHTCHIP_APP_INCLUDED_
+#define _EIGHTCHIP_APP_INCLUDED_
+
+#include "ECCpu.h"
+#include "ECCpu_Defs.h"
+
+//-------------------------------------------------------------------------------------------------
+
+class EightChipApp
+{
+public:
+    EightChipApp( );
+
+public:
+    int OnExecute( );
+    bool OnInit( );
+    void OnLoop( );
+    void OnCleanup( );
+
+private:
+    bool statusRunning;
+
+    EightChipCPU* eightchip_cpu;
+
+    SETTINGS_MAP settings;
+};
+
+//-------------------------------------------------------------------------------------------------
+
+#endif
+
+//-------------------------------------------------------------------------------------------------
---
+++ src/app/ECApp.cpp
@@ -0,0 +1,43 @@
+#include "ECApp.h"
+
+//-------------------------------------------------------------------------------------------------
+
+EightChipApp::EightChipApp( )
+{
+    statusRunning = true;
+
+    eightchip_cpu = EightChipCPU::GetInstance( );
+}
+
+//-------------------------------------------------------------------------------------------------
+
+int
+EightChipApp::OnExecute( )
+{
+    // Initialise the emulator (GFX, CPU, ...)
+    if ( OnInit( ) == false )
+        return -1;
+
+    // Executes the emulation cycles (opcodes, etc.) into a main loop
+    while ( statusRunning )
+    {
+        OnLoop( );
+    }
+
+    // Clear
+    OnCleanup( );
+
+    return 0;
+}
+
+//-------------------------------------------------------------------------------------------------
+
+int
+main( int argc, char* argv[ ] )
+{
+    EightChipApp myEmulator;
+
+    return myEmulator.OnExecute( );
+}
+
+//-------------------------------------------------------------------------------------------------
