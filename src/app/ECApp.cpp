56d02b0775517892205ea9705283188bd89d9081
Lebrazi Mohammed Achraf <lebrazi.med.achraf@gmail.com>
2022-12-13 03:23:03 +0100

    Consolidated all emulator action files into a single Cpp.

--- src/app/ECApp.cpp
+++ src/app/ECApp.cpp
@@ -1,7 +1,6 @@
 #include "ECApp.h"
 
 //-------------------------------------------------------------------------------------------------
-
 EightChipApp::EightChipApp( )
 {
     statusRunning = true;
@@ -10,34 +9,80 @@ EightChipApp::EightChipApp( )
 }
 
 //-------------------------------------------------------------------------------------------------
+bool
+EightChipApp::Initialise( )
+{
+    if ( !ecemulate::LoadSettings( this->settings ) )
+    {
+        ecsyst::LogError( ERR09 );
+        delete this->eightchip_cpu;
 
+        return false;
+    }
+
+    if ( !ecgfx::InitGraphics( this->main_window ) )
+    {
+        ecsyst::LogError( ERR04 );
+        delete this->eightchip_cpu;
+
+        return false;
+    }
+
+    if ( !ecemulate::LoadRom( this->eightchip_cpu, this->settings ) )
+    {
+        ecsyst::LogError( ERR03 );
+        SDL_Quit( );
+
+        delete this->eightchip_cpu;
+
+        return false;
+    }
+
+    return true;
+}
+
+//-------------------------------------------------------------------------------------------------
 int
-EightChipApp::OnExecute( )
+EightChipApp::Run( )
 {
     // Initialise the emulator (GFX, CPU, ...)
-    if ( OnInit( ) == false )
+    if ( Initialise( ) == false )
         return -1;
 
     // Executes the emulation cycles (opcodes, etc.) into a main loop
     while ( statusRunning )
     {
-        OnLoop( );
+        Update( );
     }
 
     // Clear
-    OnCleanup( );
+    Shutdown( );
 
     return 0;
 }
 
 //-------------------------------------------------------------------------------------------------
+void
+EightChipApp::Update( )
+{
+    ecemulate::EmulateCycle( this->eightchip_cpu, this->settings, this->statusRunning, this->main_window );
+}
 
+//-------------------------------------------------------------------------------------------------
+void
+EightChipApp::Shutdown( )
+{
+    delete this->eightchip_cpu;
+    SDL_Quit( );
+}
+
+//-------------------------------------------------------------------------------------------------
 int
 main( int argc, char* argv[ ] )
 {
     EightChipApp myEmulator;
 
-    return myEmulator.OnExecute( );
+    return myEmulator.Run( );
 }
 
 //-------------------------------------------------------------------------------------------------
